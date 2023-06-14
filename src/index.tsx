5b7f1bbb63e27ffe76fa5de491d36b1afbddfaaf
KrysDev <webdevkrys@gmail.com>
2023-06-14 22:25:21 +0100

    X

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class App extends React.Component {
+interface AppProps {
+  color?: string;
+}
+
+// interface AppState {
+//   counter: number;
+// }
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
+//     this.state = { counter: 0 };
+//   }
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  plus = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  minus = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>XxXxX</div>;
+    return (
+      <div>
+        <button onClick={this.plus}>PLUS</button>
+        <button onClick={this.minus}>MINUS</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
